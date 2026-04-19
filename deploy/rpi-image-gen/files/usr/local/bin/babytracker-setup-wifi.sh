6fcf42a4dd424efebd966336a93153a0a026afa4
Martin Bentancour <martin@viakube.com>
2026-04-19 11:07:26 +0200

    Fix Pi setup AP and add debug build option
    - NetworkManager was managing wlan0, blocking hostapd from starting the AP.
      Add /etc/NetworkManager/conf.d/99-babytracker-setup.conf to mark wlan0
      unmanaged during setup mode.
    - setup-wifi.sh removes the unmanaged config and reloads NM before
      attempting nmcli connect, handing wlan0 over for production use.
    - Add BABYTRACKER_DEBUG build flag and a workflow_dispatch input that
      enables SSH and unlocks the pi user (password: babytracker) for
      on-device troubleshooting. Debug images are not uploaded to releases.

--- deploy/rpi-image-gen/files/usr/local/bin/babytracker-setup-wifi.sh
+++ deploy/rpi-image-gen/files/usr/local/bin/babytracker-setup-wifi.sh
@@ -17,6 +17,11 @@ systemctl stop babytracker-setup-ap.service || true
 # Remove AP static IP
 ip addr del 192.168.4.1/24 dev wlan0 2>/dev/null || true
 
+# Hand wlan0 over to NetworkManager (during setup, hostapd owned it)
+rm -f /etc/NetworkManager/conf.d/99-babytracker-setup.conf
+nmcli general reload || systemctl reload NetworkManager || systemctl restart NetworkManager
+sleep 2
+
 # Connect to Wi-Fi using NetworkManager
 if [ -n "${PASSWORD}" ]; then
     nmcli dev wifi connect "${SSID}" password "${PASSWORD}" ifname wlan0
